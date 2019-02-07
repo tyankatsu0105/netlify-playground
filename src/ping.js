b55dd9be600501d03c3b4f6044057a6f89732980
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-02-08 00:14:29 +0900

    Add 通知

--- src/ping.js
+++ src/ping.js
@@ -1,5 +1,26 @@
-exports.handler = (event, context, callback) => {
-  console.log(JSON.stringify(event,null,4));
+import axios from 'axios'
+
+exports.handler = async (event, context, callback) => {
+  const webhookBody = JSON.parse(event.body);
+  console.log(webhookBody);
+
+  const data = {
+    replyToken: webhookBody.events[0].replyToken,
+    messages: [
+      {
+        type: 'text',
+        text: 'Hello Netlify Bot'
+      }
+    ]
+  }
+
+  const res = await axios.post('https://api.line.me/v2/bot/message/reply', data, {
+    headers: {
+      'Content-Type': 'appilcation/json',
+      'Authorization': `Bearer ${process.env.CHANNEL_TOKEN}`
+    }
+  })
+  
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(event)
