f6d36ba6463c095467a67d5adf8788e543e0e611
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-02-08 00:16:09 +0900

    Add reply

---
+++ src/reply.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+exports.handler = async (event, context, callback) => {
+  const webhookBody = JSON.parse(event.body);
+  console.log(webhookBody);
+
+  const data = {
+    replyToken: webhookBody.events[0].replyToken,
+    messages: [
+      {
+        type: 'text',
+        text: 'Hello Netlify Bot'
+      }
+    ]
+  }
+
+  const res = await axios.post('https://api.line.me/v2/bot/message/reply', data, {
+    headers: {
+      'Content-Type': 'appilcation/json',
+      'Authorization': `Bearer ${process.env.CHANNEL_TOKEN}`
+    }
+  })
+  
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(event)
+  }) 
+}
