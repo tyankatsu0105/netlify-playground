bc8a6265e865106640c4c8e547f0e0e16ea519e0
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-02-09 11:24:39 +0900

    Fix reply

--- src/reply.js
+++ src/reply.js
@@ -6,7 +6,6 @@ exports.handler = async function(event, context, callback) {
 
   const data = {
     replyToken: webhookBody.events[0].replyToken,
-    to: webhookBody.userId,
     messages: [
       {
         type: 'text',
